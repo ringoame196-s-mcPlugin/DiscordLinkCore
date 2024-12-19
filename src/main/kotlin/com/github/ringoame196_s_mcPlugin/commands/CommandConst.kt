ff1296c45e38101a6ff8f2e35447a33b635ff339
りんご飴196 <kazumac41@gmail.com>
2024-12-19 23:06:13 +0900

    ✨  :TabCompleter実装

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/CommandConst.kt
@@ -0,0 +1,7 @@
+package com.github.ringoame196_s_mcPlugin.commands
+
+object CommandConst {
+    const val LINK_COMMAND = "link"
+    const val UN_LINK_COMMAND = "unlink"
+    const val SHOW_COMMAND = "show"
+}
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
@@ -1,11 +1,19 @@
 package com.github.ringoame196_s_mcPlugin.commands
 
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
 
 class TabCompleter : TabCompleter {
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
-        TODO("Not yet implemented")
+        return when (args.size) {
+            1 -> mutableListOf(CommandConst.LINK_COMMAND, CommandConst.UN_LINK_COMMAND, CommandConst.SHOW_COMMAND)
+            2 -> when (args[0]) {
+                CommandConst.SHOW_COMMAND -> (Bukkit.getOnlinePlayers().map { it.name }.toMutableList())
+                else -> mutableListOf()
+            }
+            else -> mutableListOf()
+        }
     }
 }
