3fd93ce7d14e109dd60d3801db2c6abbd7440fd6
りんご飴196 <kazumac41@gmail.com>
2024-12-23 02:00:29 +0900

    ✨  :DiscordBOT起動をクラスへ

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -2,14 +2,9 @@ package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
-import com.github.ringoame196_s_mcPlugin.discord.DiscordCommandConst
-import com.github.ringoame196_s_mcPlugin.discord.SlashCommandEvent
+import com.github.ringoame196_s_mcPlugin.data.Data
+import com.github.ringoame196_s_mcPlugin.managers.DiscordManager
 import com.github.ringoame196_s_mcPlugin.managers.LinkManager
-import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.Activity
-import net.dv8tion.jda.api.interactions.commands.OptionType
-import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
@@ -19,57 +14,16 @@ class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
 
-        val linkManager = LinkManager(plugin)
+        Data.plugin = plugin
 
         saveDefaultConfig() // config生成
-        bootDiscordBOT()
+        DiscordManager().bootDiscordBOT(plugin)
 
         if (!File(plugin.dataFolder, "data.db").exists()) saveResource("data.db", false)
-        linkManager.resetAuthKey()
+        LinkManager().resetAuthKey()
 
         val command = getCommand("dcore")
-        command!!.setExecutor(Command(plugin))
+        command!!.setExecutor(Command())
         command.tabCompleter = TabCompleter()
-    }
-
-    private fun bootDiscordBOT() {
-        val config = plugin.config
-
-        val token = config.getString("token")
-        val activity = config.getString("activity") ?: "Minecraft"
-
-        var jda: JDA? = null
-
-        try {
-            if (token != "" && token != null) { // tokenが設定されていないと 実行しない
-                val jdaBuilder = JDABuilder.createDefault(token)
-                jdaBuilder.setActivity(Activity.playing(activity))
-                jda = jdaBuilder.addEventListeners(SlashCommandEvent(plugin)).build() // bot起動
-            } else {
-                val message = "Token not set"
-                logger.info(message) // token未設定時にメッセージを出す
-            }
-        } catch (e: Exception) {
-            val errorMessage = "BOT起動中にエラーが発生しました。 ${e.message}"
-            logger.info(errorMessage) // エラー文出力
-        }
-
-        jda ?: return // jdaが設定されていなければ処理終了
-        // DiscordBOTコマンド登録
-        val commands = mutableListOf(
-            CommandData(DiscordCommandConst.LINK_COMMAND, DiscordCommandConst.LINK_COMMAND_LORE)
-                .addOption(
-                    OptionType.STRING,
-                    DiscordCommandConst.AUTH_KEY_OPTION,
-                    DiscordCommandConst.AUTH_KEY_OPTION_LORE,
-                    true
-                )
-        )
-
-        jda.updateCommands().addCommands(*commands.toTypedArray()).queue {
-            logger.info("コマンドの登録が完了しました")
-        }
-
-        Data.jda = jda
     }
 }
