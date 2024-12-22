8ed15f5b397b7c2269214f268f69e69f3ed86669
りんご飴196 <kazumac41@gmail.com>
2024-12-22 21:36:22 +0900

    ✨  :データベース関係追加

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/DBConstData.kt
@@ -0,0 +1,11 @@
+package com.github.ringoame196_s_mcPlugin
+
+object DBConstData {
+    const val TABLE_NAME = "linkdata"
+
+    const val MC_UUID_KEY = "mc_uuid"
+    const val MC_NAME_KEY = "mc_name"
+    const val DISCORD_ID_KEY = "discord_id"
+    const val IS_VERIFIED_KEY = "is_verified"
+    const val AUTH_KEY_KEY = "auth_key"
+}
