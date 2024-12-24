7060a92db0877f9ab3547fb7b05c121808036c1c
りんご飴196 <kazumac41@gmail.com>
2024-12-24 23:26:12 +0900

    ✨  :外部JDAを設定できるように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/LinkManagerAPI.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/LinkManagerAPI.kt
@@ -1,11 +1,16 @@
 package com.github.ringoame196_s_mcPlugin
 
+import com.github.ringoame196_s_mcPlugin.data.Data
 import com.github.ringoame196_s_mcPlugin.managers.LinkManager
+import net.dv8tion.jda.api.JDA
 import org.bukkit.OfflinePlayer
 
 object LinkManagerAPI {
     private fun linkManager() = LinkManager()
 
+    fun setJDA(jda: JDA) {
+        Data.jda = jda
+    }
     fun acquisitionMcName(discordID: String): String? = linkManager().acquisitionMcName(discordID)
     fun acquisitionMcUUD(discordID: String): String? = linkManager().acquisitionMcUUD(discordID)
     fun acquisitionDiscordID(mcUUID: String): String? = linkManager().acquisitionDiscordID(mcUUID)
